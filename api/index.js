22f6f358cc174dbacf8e80acf82c10af311d57ac
Ali Meral <meralmuhammet544@gmail.com>
2024-08-07 14:56:47 +0300

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -6,6 +9,15 @@ app.get("", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(4000, () => {
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
