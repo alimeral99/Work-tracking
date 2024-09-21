669198489404861b6770f739e94503d8548f7b19
Ali Meral <meralmuhammet544@gmail.com>
2024-09-21 15:12:54 +0300

    Set up redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import worksReducer from "./Works/WorksSlice";
 import userReducer from "./User/UserSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 import comparisonWorksReducer from "./Works/ComparisonWorkSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
     works: worksReducer,
     comparisonWorks: comparisonWorksReducer,
-    user: userReducer,
+    user: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
